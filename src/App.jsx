11b7b6e2dc28ef7003c0cd5213611a15d091e31c
blueberryenjoyer <phoenix.wheatley@gmail.com>
2023-06-06 12:43:23 -0400

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import ContactList from "./components/ContactList.jsx"
-import { useState } from 'react'
 
 export default function App() {
-  const [selectedContactId, setSelectedContactId] = useState(null)
 
   return (
     <>
       {
-        <ContactList setSelectedContactId={setSelectedContactId} selectedContactId={selectedContactId} />
+        <ContactList />
       }
     </>
   );
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react'
 import dummy from './DummyData.jsx'
+import './puppy.css'
 
 //because i could not figure out the name of our cohort, i used dummy data from the documentation
 //i cannot complete the api call section of this project.
 
-export default function ContactList({ setSelectedContactId, selectedContactId }) {
+export default function ContactList(props) {
     const [contacts, setContacts] = useState(0)
     const [contacts2, setContacts2] = useState(0)
 
     useEffect(() => {
-        async function fetchContacts() {
+        function fetchContacts() {
             try {
                 const response = dummy()
                 const responseteam1 = response.data.teams[0]
@@ -20,45 +21,40 @@ export default function ContactList({ setSelectedContactId, selectedContactId })
                 const arr2 = Object.values(responseteam2.players);
                 setContacts(arr1)
                 setContacts2(arr2)
-                console.log(contacts)
-                //why does this log 0? if i save this and it auto-updates, it becomes the array i want.
-                //i dont think i understand state, or async, or something else i am missing
 
             } catch (error) {
                 console.error(error);
             }
         }
         fetchContacts()
-        console.log(contacts) //this also logs 0
     }, []);
+
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th colSpan="3">team1</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        dogsss
-                    </td>
-                </tr>
-            </tbody>
-
-            <thead>
-                <tr>
-                    <th colSpan="3">team2</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        dogs
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-
+        <div className="contacts-container">
+            <div id="team1">
+                <h2>team 1:</h2>
+                <ul>
+                    {contacts.length > 0 ? (
+                        contacts.map((contact, index) => (
+                            <li key={index}>{contact.name}</li>
+                        ))
+                    ) : (
+                        <li>Loading team 1...</li>
+                    )}
+                </ul>
+            </div>
+            <div id="team2">
+                <h2 >team 2:</h2>
+                <ul>
+                    {contacts2.length > 0 ? (
+                        contacts2.map((contact, index) => (
+                            <li key={index}>{contact.name}</li>
+                        ))
+                    ) : (
+                        <li>Loading team 2...</li>
+                    )}
+                </ul>
+            </div>
+        </div>
     );
 }
